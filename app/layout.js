5c164c6c99d6e3c920478a7b8f00b549685217de
batumzff <batuhanmuzafferoglu@gmail.com>
2024-01-31 23:47:14 +0300

    page

---
+++ app/layout.js
@@ -0,0 +1,25 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import AuthContextProvider from "@/context/AuthContext";
+
+const inter = Inter({ subsets: ["latin"] });
+
+//! metadata objesi ve generateMetadata fonksiyonu sadece Server Componentlerden export edilebilir.
+export const metadata = {
+  title: "Netflix",
+  description: "a movie platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthContextProvider>
+          <Navbar />
+          {children}
+        </AuthContextProvider>
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "gray-dark-main": "#23242a",
+        "gray-dark-second": "#28292d",
+        "gray-light": "#d3dce6",
+        "red-main": "#ff4b45",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [],
+};
