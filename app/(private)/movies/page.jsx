707c643c72e80426f747272df9c6921fc79b1f01
batumzff <batuhanmuzafferoglu@gmail.com>
2024-02-01 22:53:23 +0300

    moviecard

--- app/(private)/movies/page.jsx
+++ app/(private)/movies/page.jsx
@@ -1,10 +1,23 @@
 import { getMovies } from "@/helpers/movieFunctions";
 import React from "react";
+import HeroSection from "./components/HeroSection";
+import MovieSection from "./components/MovieSection";
 
 const Movies = async () => {
   const movies = await getMovies("now_playing");
-  console.log(movies);
-  return <div>Movies</div>;
+  //   console.log(movies);
+  return (
+    <>
+      <HeroSection
+        movieId={movies[0].id}
+        title={movies[0].title}
+        overview={movies[0].overview}
+      />
+      <div className="px-4 md:px12 mt-4">
+        <MovieSection title="NOW PLAYING" type="now_playing" />
+      </div>
+    </>
+  );
 };
 
 export default Movies;
